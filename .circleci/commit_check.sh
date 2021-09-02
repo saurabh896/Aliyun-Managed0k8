10f57851e1494ac0d578d3bdaca6366603bbf181
Saurabh Kadam <saurabhkdm721@gmail.com>
2021-09-02 15:38:14 +0530

    Create commit_check.sh

---
+++ .circleci/commit_check.sh
@@ -0,0 +1,23 @@
+set -e
+
+# latest commit
+LATEST_COMMIT=$(git rev-parse HEAD)
+
+# latest commit where path/to/folder1 was changed
+FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff K8)
+
+# latest commit where path/to/folder2 was changed
+FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff RAM)
+
+if [ $FOLDER1_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "files in folder1 has changed"
+        .circleci/do_something.sh
+elif [ $FOLDER2_COMMIT = $LATEST_COMMIT ];
+    then
+        echo "files in folder2 has changed"
+        .circleci/do_something_else.sh
+else
+     echo "no folders of relevance has changed"
+     exit 0;
+fi
