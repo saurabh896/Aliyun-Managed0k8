037d6abe89ca2c041644fef9365972009ebf4d87
Saurabh Kadam <saurabhkdm721@gmail.com>
2021-09-02 15:42:47 +0530

    Update main.tf

--- K8/main.tf
+++ K8/main.tf
@@ -2,6 +2,7 @@ provider "alicloud" {
     region = var.region != "" ? var.region: "cn-beijing"
 }
 
+
 resource "alicloud_cs_managed_kubernetes" "this" {
   count = length(local.vswitch_ids) > 0 ? 1 :0
   name = local.k8_name
